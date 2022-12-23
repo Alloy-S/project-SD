3b67f881f41d329a7da1414585a05e2b4e80246a
Alloy-S <107180700+Alloy-S@users.noreply.github.com>
2022-12-23 17:02:32 +0700

    update lagi

--- coba.js
+++ coba.js
@@ -8,10 +8,10 @@ class GraphMatrix {
     add_vertex() {
         for (var row, _pj_c = 0, _pj_a = this.matrix, _pj_b = _pj_a.length; _pj_c < _pj_b; _pj_c += 1) {
             row = _pj_a[_pj_c];
-            row.append(0);
+            row.push(0);
         }
 
-        this.matrix.append(function () {
+        this.matrix.push(function () {
             var _pj_a = [],
                 _pj_b = this.count + 1;
 
@@ -40,7 +40,8 @@ class GraphMatrix {
 
             for (var edge, _pj_f = 0, _pj_d = vertex, _pj_e = _pj_d.length; _pj_f < _pj_e; _pj_f += 1) {
                 edge = _pj_d[_pj_f];
-                console.log(edge);
+                // console.log(edge);
+                process.stdout.write(edge);
             }
 
             console.log();
@@ -90,11 +91,11 @@ class GraphMatrix {
 
     dijkstra(src, end) {
         var distance, explore, path, x;
-        distance = [sys.maxsize] * this.count;
-        distance[this.vertices[src]] = 0;
+        distance = [Number.MAX_VALUE] * this.count;
+        distance[src] = 0;
         path = [null] * this.count;
         explore = [false] * this.count;
-        path[this.vertices[src]] = -1;
+        path[src] = -1;
 
         for (var cout = 0, _pj_a = this.count; cout < _pj_a; cout += 1) {
             x = this.minDistance(distance, explore);
@@ -113,17 +114,121 @@ class GraphMatrix {
 
 
 }
+let V = 4;
+function minDistance(dist, sptSet) {
+
+    // Initialize min value
+    let min = Number.MAX_VALUE;
+    let min_index = -1;
+
+    for (let v = 0; v < V; v++) {
+        if (sptSet[v] == false && dist[v] <= min) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+// A utility function to print
+// the constructed distance array
+function printSolution(dist) {
+    document.write("Vertex \t\t Distance from Source<br>");
+    for (let i = 0; i < V; i++) {
+        document.write(i + " \t\t " +
+            dist[i] + "<br>");
+    }
+}
+
+// Function that implements Dijkstra's
+// single source shortest path algorithm
+// for a graph represented using adjacency
+// matrix representation
+function dijkstra(graph, src) {
+    let dist = new Array(V);
+    let sptSet = new Array(V);
+
+    // Initialize all distances as
+    // INFINITE and stpSet[] as false
+    for (let i = 0; i < V; i++) {
+        dist[i] = Number.MAX_VALUE;
+        sptSet[i] = false;
+    }
+
+    // Distance of source vertex
+    // from itself is always 0
+    dist[src] = 0;
+
+    // Find shortest path for all vertices
+    for (let count = 0; count < V - 1; count++) {
+
+        // Pick the minimum distance vertex
+        // from the set of vertices not yet
+        // processed. u is always equal to
+        // src in first iteration.
+        let u = minDistance(dist, sptSet);
+
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+
+        // Update dist value of the adjacent
+        // vertices of the picked vertex.
+        for (let v = 0; v < V; v++) {
+
+            // Update dist[v] only if is not in
+            // sptSet, there is an edge from u
+            // to v, and total weight of path
+            // from src to v through u is smaller
+            // than current value of dist[v]
+            if (!sptSet[v] && graph[u][v] != 0 &&
+                dist[u] != Number.MAX_VALUE &&
+                dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    // Print the constructed distance array
+    printSolution(dist);
+
+    let index = 0;
+    var min = Number.MAX_VALUE;
+
+    for (let i = 0; i < dist.length; i++) {
+        // console.log(i, end.includes(i));
+        if (dist[i] < min && end.includes(i)) {
+
+            min = dist[i];
+            index = i;
+        }
+        // const element = hasil[i];
+    }
+
+
+    return index;
+}
 
 
 
 let graph = new GraphMatrix();
-graph.add_vertex();
-graph.add_vertex();
-graph.add_vertex();
-graph.add_vertex();
-graph.add_edge(0, 1);
-graph.add_edge(1, 2);
-graph.add_edge(2, 3);
-graph.add_edge(1, 3);
+let end = [1, 2, 3]
+graph.matrix = [
+    [0, 1, 0, 1],
+    [1, 0, 1, 0],
+    [0, 1, 0, 1],
+    [1, 0, 0, 0]
+]
+// graph.add_vertex();
+// graph.add_vertex();
+// graph.add_vertex();
+// graph.add_vertex();
+// graph.add_edge(0, 1);
+// graph.add_edge(1, 2);
+// graph.add_edge(2, 3);
+// graph.add_edge(1, 3);
 
-graph.print_adjacency_matrix();
+
+
+let hasil = dijkstra(graph.matrix, 0);
+
+console.log(hasil);
