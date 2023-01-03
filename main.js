0673d0c167315b002bbfc51d103b13c03ad80c1e
Alloy-S <107180700+Alloy-S@users.noreply.github.com>
2023-01-03 23:51:02 +0700

    rapi" menuju final

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var graphMatrix, position;
+var graphMatrix, position, wall;
 var win = false;
 let end = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 17, 26, 35, 44, 53, 62, 71, 80, 79, 78, 77, 76, 75,
   74, 73, 73, 72, 63, 54, 45, 36, 27, 18, 9,
 ];
-let wall;
-let V = 8;
 
+
+// untuk mendekteksi tiles di click
 $(".green").click(function () {
   let btn = $(this);
   if (btn.attr("id") != position) {
@@ -17,13 +17,12 @@ $(".green").click(function () {
   }
 });
 
-function minDistance(dist, sptSet, V) {
-  // Initialize min value
+function minDistance(dist, explore, V) {
   let min = Number.MAX_VALUE;
   let min_index = -1;
 
   for (let v = 0; v < V; v++) {
-    if (sptSet[v] == false && dist[v] <= min) {
+    if (explore[v] == false && dist[v] <= min) {
       min = dist[v];
       min_index = v;
     }
@@ -31,22 +30,21 @@ function minDistance(dist, sptSet, V) {
   return min_index;
 }
 
-function printSolution(dist, path, src, dest, V) {
-  // document.write("Vertex \t\t Distance from Source<br>");
-  $("tbody").html("");
-  var check = true;
+function printSolution(dist, path, dest, V) {
+  // $("tbody").html("");
   var count = 0;
   for (let i = 0; i < V; i++) {
     if (end.includes(i)) {
+      // jika semua isi dari array distance berisi max value berarti tidak ada shortest path menuju keluar
       if (dist[i] == Number.MAX_VALUE) {
-        check = false;
         count = count + 1;
       }
-      $("tbody").append("<tr><td>" + i + "</td><td>" + dist[i] + "</td></tr>");
+      // $("tbody").append("<tr><td>" + i + "</td><td>" + dist[i] + "</td></tr>");
     }
   }
   
-  if (check == false && count == end.length-1) {
+  // pengecekan untuk menang
+  if (count == end.length-1) {
       setTimeout(function () {
         setTimeout(function () {
           $("#win").css("display", "block");
@@ -58,21 +56,17 @@ function printSolution(dist, path, src, dest, V) {
         console.log("kalah123");
         $("#win").css("display", "block");
       }, 500);
+
+      return 0;
   }
 
   let finalPath = [];
 
   for (let i = 0; i < V; i++) {
     if (i == dest) {
-      // document.write("distance to " + i + ":" + dist[i] + "<br>");
-      // document.write("path: ");
-
-      printPath(i, path, finalPath);
-
-      // document.write("<br>");
+      getPath(i, path, finalPath);
     }
   }
-
   return finalPath;
 }
 
@@ -83,18 +77,18 @@ function delEdge(adj, vertex) {
   }
 }
 
-function printPath(current, path, finalPath) {
+function getPath(current, path, finalPath) {
   if (current == -1) {
     return;
   }
   // console.log(finalPath);
-  printPath(path[current], path, finalPath);
+  getPath(path[current], path, finalPath);
   finalPath.push(current);
   // document.write(current + "-->");
 }
 
 function dijkstra(graph, src) {
-  console.log(graph.length);
+  // console.log(graph.length);
   let V = graph.length;
   let dist = new Array(V);
   let explore = new Array(V);
@@ -111,12 +105,8 @@ function dijkstra(graph, src) {
 
 
   for (let count = 0; count < V - 1; count++) {
-
     let u = minDistance(dist, explore, V);
-
-
     explore[u] = true;
-
 
     for (let v = 0; v < V; v++) {
       if (!explore[v] && graph[u][v] != 0 && dist[u] != Number.MAX_VALUE && dist[u] + graph[u][v] < dist[v]) {
@@ -140,15 +130,10 @@ function dijkstra(graph, src) {
     }
   }
 
-  return printSolution(dist, path, src, index, V);
+  return printSolution(dist, path, index, V);
 }
 
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
-
 function newGame(max, setWall) {
-
   wall = setWall;
   $(".green").css("background", "#ccff00");
   $("#40").css("background", "red");
@@ -156,9 +141,9 @@ function newGame(max, setWall) {
   document.getElementById('40').style.backgroundSize = "58px 62px";
   document.getElementById('40').style.backgroundPosition = "center";
   document.getElementById('40').style.backgroundRepeat = "no-repeat";
+
   $(".green").prop("disabled", false);
   let vertex = Math.pow(max, 2);
-  //   let max = 4;
   let matrix = new Array(vertex);
 
   for (let i = 0; i < vertex; i++) {
@@ -175,16 +160,16 @@ function newGame(max, setWall) {
   let count = 0;
   for (let i = 0; i < vertex; i++) {
     if (i + 1 < level + max) {
-      console.log(i, level);
+      // console.log(i, level);
       if (i + 1 < vertex) {
-        console.log("edge + 1", i);
+        // console.log("edge + 1", i);
         matrix[i][i + 1] = 1;
         matrix[i + 1][i] = 1;
       }
     }
 
     if (i + max < vertex) {
-      console.log("edge + 9", i);
+      // console.log("edge + 9", i);
       matrix[i][i + max] = 1;
       matrix[i + max][i] = 1;
     }
@@ -197,7 +182,6 @@ function newGame(max, setWall) {
   }
 
   // untuk mengatur jumlah wall
-  // wall = 5;
   for (let i = 0; i < wall; i++) {
     var id_random = Math.floor(Math.random() * 81);
     if (id_random != 40) {
@@ -208,60 +192,32 @@ function newGame(max, setWall) {
       i--;
     }
   }
-  // for (let i = 0; i < wall; i++) {
-  //   let random = getRandomInt(vertex);
-  // let col = getRandomInt(vertex);
-  //   if (random != 40) {
-  // delEdge(matrix, random);
-  // if (matrix[row][col] != 0) {
-  //   matrix[row][col] = 0
-  //   level--;
-  // }
-  // btn.css('background', '#748404');
-  // btn.prop("disabled", true);
-  //     console.log(random)
-  //     id = "#" + random;
-  //     $(id).css('background', '#748404');
-  //     $(id).prop("disabled", true);
-  //   } else {
-  //     i--;
-  //   }
-  // }
+
+  // set posisi mula-mula dan set graphnya
   position = 40;
   graphMatrix = matrix;
   // return matrix;
 }
 
 function movePlayer() {
+  // menjalan kan algoritma djikstra dan mereturn path paling pendek
   let hasil = dijkstra(graphMatrix, position);
 
   if (hasil != 0) {
-  var str;
-  for (let i = 0; i < hasil.length; i++) {
-    str += hasil[i] + "->";
-  }
+  // var str;
+  // for (let i = 0; i < hasil.length; i++) {
+  //   str += hasil[i] + "->";
+  // }
 
   let id = "#" + position;
   let from = position;
   $(id).css("background", "#ccff00");
-  $(".path").html(str);
+  // $(".path").html(str);
   $(".position").html(position);
   position = hasil[1];
   let next = hasil[1];
   id = "#" + position;
   $(id).css("background", "red");
-
-  // $(id).append("<img id='character' class='character'>");
-
-  // if(from - next == 1) {
-  //   document.getElementById('character').className = 'robber-left';
-  // } else if (from - next == -1) {
-  //   document.getElementById('character').className = 'robber-right';
-  // } else if (from - next == 9) {
-  //   document.getElementById('character').className = 'robber-up';
-  // } else if (from - next == -9) {
-  //   document.getElementById('character').className = 'robber-down';
-  // }
 
   $(".character").css("position", "absolute");
   if (from - next == 1) {
@@ -274,13 +230,13 @@ function movePlayer() {
     document.getElementById(position).style.backgroundImage = "url('assets/char_maju_fix.png')";
   }
 
-  // document.getElementById(position).style.backgroundImage="url('idle.png')";
   document.getElementById(position).style.backgroundSize = "58px 62px";
   document.getElementById(position).style.backgroundPosition = "center";
   document.getElementById(position).style.backgroundRepeat = "no-repeat";
-  // document.getElementById(position).style.textIndent="-999px";
-  // document.getElementById(position).style.fontSize="0";
-  console.log(graphMatrix);
+ 
+  // console.log(graphMatrix);
+
+  //mengecek apakah thief berada di pinggir map
   if (end.includes(position)) {
     setTimeout(function () {
       setTimeout(function () {
@@ -297,13 +253,6 @@ function movePlayer() {
 }
 };
 
-//   console.log("sebelum delete");
-
-//   delEdge(matrix, 6,7);
-
-// let matrix = newGame(9)
-// let position = 8;
+// inisialisasi awal
 newGame(9, 5);
 console.log(graphMatrix);
-// let hasil = dijkstra(graphMatrix, position);
-// position = hasil[1];
